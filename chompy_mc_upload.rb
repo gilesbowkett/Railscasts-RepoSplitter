4649096d0c06bbf83d2c00b98a406112ed7ffbed
Giles Bowkett <gilesb@gmail.com>
2011-07-29 13:25:56 -0700

    renamed, added ability to find dirs

--- chompy_mc_upload.rb
+++ chompy_mc_upload.rb
@@ -1,11 +1,29 @@
 %w{yaml rubygems rest_client}.each {|lib| require lib}
 
-github_credentials = YAML::load(File.open("github.yml"))
+class ChompyMcUpload
+  def initialize
+    @github_credentials = YAML::load(File.open("github.yml"))
+    @railscast_repos = "/Users/giles/dev/railscasts-episodes"
+  end
 
-begin
-  new_repo = github_credentials.merge :public => 1, :name => "railscasts/asdf23"
-  RestClient.post("https://github.com/api/v2/json/repos/create", new_repo)
-rescue RestClient::ResourceNotFound
-  puts 404
+  def create(repo_name)
+    begin
+      new_repo = github_credentials.merge :public => 1, :name => "railscasts/#{repo_name}"
+      RestClient.post("https://github.com/api/v2/json/repos/create", new_repo)
+    rescue RestClient::ResourceNotFound
+      puts 404
+    rescue SocketError
+      puts "probable wifi fail"
+    end
+  end
+
+  def list
+    Dir.new(@railscast_repos).collect do |railscast_repo|
+      next unless (episode = railscast_repo.match /episode-(\d+)/)
+      episode.chomp
+    end
+  end
 end
+
+ChompyMcUpload.new.list
 
